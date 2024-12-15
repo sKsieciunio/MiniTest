0bf32a490c0a6ff503e46ba0aebd71f1a3649c2d
Hubert Blonowski <135438393+sKsieciunio@users.noreply.github.com>
2024-12-15 13:32:44 +0100

    feat: Attributes added

---
+++ MiniTest/MiniTest/Attributes.cs
@@ -0,0 +1,37 @@
+﻿namespace MiniTest;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class TestClassAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Method)]
+public class TestMethodAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Method)]
+public class BeforeEachAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Method)]
+public class AfterEachAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Method)]
+public class PriorityAttribute : Attribute
+{
+    public int Priority { get; }
+    public PriorityAttribute(int priority) => Priority = priority;
+}
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public class DataRowAttribute : Attribute
+{
+    public object?[] Parameters { get; } 
+    public string Description { get; }
+
+    public DataRowAttribute(string description, params object?[] parameters)
+    {
+        Parameters = parameters;
+        Description = description;
+    }
+
+    public DataRowAttribute(params object?[] parameters) : this(string.Empty, parameters) { }
+}
+
+
